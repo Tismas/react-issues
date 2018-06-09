f86a46ede8236ff797219448dc08044f3992d06c
Nasos <adamski256@gmail.com>
2018-06-09 14:35:50 +0200

    more interesting loader and build

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -4,10 +4,31 @@ import '../../styles/common/loader.scss';
 
 
 class Loader extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dotsCount: 1
+        };
+
+        this.refreshDots = this.refreshDots.bind(this);
+    }
+    
+    componentDidMount() {
+        this.animationInterval = setInterval(this.refreshDots, 500);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.animationInterval);
+    }
+
+    refreshDots() {
+        this.setState({ dotsCount: this.state.dotsCount % 3 + 1 });
+    }
+
     render() {
         return (
-            <div>
-                Loading...                
+            <div className="loader">
+                Loading{'.'.repeat(this.state.dotsCount)}
             </div>
         );
     }
