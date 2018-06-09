3bf16d8bc9824df366f2e4f82044ec96c4586701
Nasos <adamski256@gmail.com>
2018-06-09 11:30:45 +0200

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader?data=@import './src/styles/variables.scss';"]
+      },
+      {
+        test: /\.png/,
+        use: 'file-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '_build')
+  }
+};
